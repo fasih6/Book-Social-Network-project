95dbe3806374f529ed0744d0c559b14e64f4dd5f
fasih6 <fasih8576@gmail.com>
2025-12-05 16:07:10 +0100

    added docker

--- book-network/src/main/java/com/myproject/book/security/SecurityConfig.java
+++ book-network/src/main/java/com/myproject/book/security/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.*;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -95,7 +96,7 @@ public class SecurityConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
